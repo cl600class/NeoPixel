7e39749af46488cbea8633327474226b2cbdcd9e
cl600class <b06901051@ntu.edu.tw>
2019-02-01 02:19:05 +0800

    add more testing effect

--- RGBMatrix/RGBMatrix.ino
+++ RGBMatrix/RGBMatrix.ino
@@ -43,8 +43,9 @@ int x = matrix.width();
 int y = matrix.height();
 int brightness = 40;
 uint16_t color_w = matrix.Color(255,255,255);
-uint16_t color_b = matrix.Color(0,0,0);
+uint16_t color_bk = matrix.Color(0,0,0);
 uint16_t color_y = matrix.Color(255,140,0);
+uint16_t color_bl = matrix.Color(0,0,255);
 
 void setup() {
   Serial.begin(9600);
@@ -63,6 +64,11 @@ void set(){
   fill();
   fade_out(millis());
   single_point();
+  single_point_t();
+  dual();
+  fade_out(millis());
+  highlight();
+  //turn_light();
 }
 
 void fill_srow(){
@@ -103,15 +109,15 @@ void fade_out(unsigned long _last){
 }
 
 void single_point(){
-  int x_c[] = {0,0,0,1,2,2,2,3,4,4,4,5,6,6,6,7,8,8,8,9,10,10,10,11,12,12,12,13,14};
-  int y_c[] = {0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0,0,1,2,2,2};
+  int x_c[] = {0,0,0,1,2,2,2,3,4,4,4,5,6,6,6,7,8,8,8,9,10,10,10,11,12,12,12,13,14,14,14,15};
+  int y_c[] = {0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0};
   int s = sizeof(x_c)/sizeof(x_c[0]);
   if (s == sizeof(y_c)/sizeof(y_c[0])){
     for(int i=0; i<s; i++){
       matrix.drawPixel(x_c[i], y_c[i], color_y);
-      Serial.println(x_c[i]);
+      //Serial.println(x_c[i]);
       if (i>0){
-        matrix.drawPixel(x_c[i-1], y_c[i-1], color_b);
+        matrix.drawPixel(x_c[i-1], y_c[i-1], color_bk);
       }
       matrix.show();
       delay(100);
@@ -120,4 +126,129 @@ void single_point(){
     delay(500);
   }
 }
+
+void single_point_t(){
+  int x_c[] = {0,0,0,1,2,2,2,3,4,4,4,5,6,6,6,7,8,8,8,9,10,10,10,11,12,12,12,13,14,14,14,15};
+  int y_c[] = {0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0,0,1,2,2,2,1,0,0};
+  int s = sizeof(x_c)/sizeof(x_c[0]);
+  if (s == sizeof(y_c)/sizeof(y_c[0])){
+    for(int i=0; i<s; i++){
+      matrix.drawPixel(x_c[i], y_c[i], color_y);
+      //Serial.println(x_c[i]);
+      matrix.show();
+      delay(100);
+    }
+    matrix.fillScreen(0);
+    delay(500);
+  }
+}
+
+void dual(){
+  int x_c[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+  int y_c[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+  int s = sizeof(x_c)/sizeof(x_c[0]);
+  int i, j = 0;
+  bool nstop = true;
+  while (nstop){
+    if (i<x){
+      matrix.drawPixel(i, 0, color_w);
+      i++;
+    }
+    if (j<s){
+      matrix.drawPixel(x_c[j], y_c[j], color_bl);
+      j++;
+    }
+    nstop = i<x or j<s;
+    matrix.show();
+    delay(100);
+  }
+}
+
+//highlight effect for only 16*3 matrix
+void highlight(){
+  //single row effect
+  int max_brightness = 255;
+  uint16_t temp_w = matrix.Color(80, 65, 80);
+  matrix.setBrightness(max_brightness);
+  for (int j=0; j<3; j++){
+    for (int i=0; i<8; i++){
+      matrix.drawPixel(i, j, temp_w);
+      matrix.show();
+      delay(50);
+    }
+  }
+  //fade in
+  unsigned long now;
+  unsigned long last = millis();
+  uint16_t temp_b;
+  int tmp = 0;
+  while (tmp <= 255){
+     now = millis();
+     if (now-last>10){
+        temp_b = matrix.Color(tmp, tmp, tmp);
+        tmp += 1;
+        matrix.drawPixel(11, 1, temp_b);
+        matrix.drawPixel(12, 1, temp_b);
+        matrix.show();
+        last = now;
+    }
+  }
+  while (tmp >= 100){
+     now = millis();
+     if (now-last>10){
+        tmp -= 1;
+        temp_b = matrix.Color(tmp, tmp, tmp);
+        matrix.drawPixel(11, 1, temp_b);
+        matrix.drawPixel(12, 1, temp_b);
+        matrix.show();
+        last = now;
+    }
+  }
+    while (tmp <= 255){
+     now = millis();
+     if (now-last>10){
+        temp_b = matrix.Color(tmp, tmp, tmp);
+        tmp += 1;
+        matrix.drawPixel(11, 1, temp_b);
+        matrix.drawPixel(12, 1, temp_b);
+        matrix.show();
+        last = now;
+    }
+  }
+  delay(1000);
+  matrix.fillScreen(0);
+  matrix.setBrightness(brightness);
+}
+
+void turn_light(){
+  int n = 1;
+  //blinking
+  while (n<=5){
+    for(int i=0; i<8; i++){
+      matrix.drawPixel(i, 1, color_y);
+    }
+    matrix.show();
+    delay(400);
+    matrix.fillScreen(0);
+    matrix.show();
+    delay(400);
+    n++;
+  }
+  n = 1;
+  //flow
+  while (n<=5){
+    for(int i=0; i<8; i++){
+      matrix.drawPixel(7-i, 1, color_y);
+      matrix.show();
+      delay(50);
+    }
+    delay(200);
+    matrix.fillScreen(0);
+    matrix.show();
+    delay(200);
+    n++;
+  }
+  delay(200);
+}
+
 
